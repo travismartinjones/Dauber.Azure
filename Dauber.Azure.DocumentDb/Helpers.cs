f5f34f6d2e4fb22095e6987ee4951ed0537cf9aa
Travis Jones <travis@dauberapp.com>
2018-02-12 16:20:26 -0600

    Add helper method for selecting all properties at once.

---
+++ Dauber.Azure.DocumentDb/Helpers.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Reflection;
+using Dauber.Core.Contracts;
+using Newtonsoft.Json;
+
+namespace Dauber.Azure.DocumentDb
+{
+    public static class Helpers
+    {
+        public static string GetPropertySelectNames<T>(string prefix = "c") where T : ViewModel
+        {
+            return $"{prefix}.{string.Join($", {prefix}.", typeof(T).GetProperties().Select(p => p.GetCustomAttribute<JsonPropertyAttribute>()).Select(jp => jp.PropertyName))}";
+        }
+    }
+}
