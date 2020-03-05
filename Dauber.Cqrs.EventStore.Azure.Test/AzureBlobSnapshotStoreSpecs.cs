eec7aec72103fa18dc914e732802cb31aea7390d
Travis Jones <travis@dauberapp.com>
2020-03-05 13:24:13 -0600

    Allow oversized snapshots.

---
+++ Dauber.Cqrs.EventStore.Azure.Test/AzureBlobSnapshotStoreSpecs.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dauber.Azure.Blob;
+using Dauber.Azure.Blob.Contracts;
+using Dauber.Core.Container;
+using Dauber.Core.Contracts;
+using HighIronRanch.Azure.TableStorage;
+using HighIronRanch.Cqrs.EventStore.Azure;
+using NUnit.Framework;
+using Shouldly;
+using SimpleCqrs.Domain;
+
+namespace Dauber.Cqrs.EventStore.Azure.Test
+{
+    public class BlobSettings : IBlobSettings
+    {
+        public string ConnectionString => "DefaultEndpointsProtocol=https;AccountName=dauberfleetblobdev;AccountKey=vSikpaLJIaEUNRJXJpbeREUyXxbgaJy2iZHLjT15NoZu9I4Ug1A52dpsCj2nGG79h/JHNCVVZxewILYxCxckHg==;EndpointSuffix=core.windows.net";
+        public bool IsContainerCreatedIfMissing => false;
+    }
+
+    public class AzureTableSettings : IAzureTableSettings
+    {
+        public string AzureStorageConnectionString => "DefaultEndpointsProtocol=https;AccountName=dauberfleetdeves;AccountKey=c8LQs9btJsfNkdGHRLwZHkynP6MyPsHwtZkkQ7Xh/2kj0YHDeaurDVKwPRcD5NfxhW9XFG1rEUXoEXPMarsbOw==";
+    }
+
+    public class SnapshotContent : Snapshot
+    {
+        public List<string> Content { get; set; } = new List<string>();
+        
+        public void SetToLength(int length)
+        {
+            for(var i=0; i < length; i++)
+                Content.Add(Guid.NewGuid().ToString());
+        }
+    }
+
+    public class AzureBlobSnapshotStoreSpecs
+    {
+        [Test]
+        public async Task when_the_snapshot_is_under_table_storage_limits_it_should_save_and_load_to_table_storage()
+        {
+            var blobStore = new BlobStore(new BlobSettings());
+            var azureBlobStore = new AzureBlobSnapshotStore(blobStore);
+            var azureTableService = new AzureTableService(new AzureTableSettings());
+            var sut = new AzureTableSnapshotStore(
+                azureBlobStore,
+                new AzureSnapshotBuilder(azureBlobStore),
+                azureTableService,
+                new DomainEntityTypeBuilder()
+            );
+            var item = new SnapshotContent {AggregateRootId = new Guid("b87bc654-3d76-4a46-ab58-c71c16b6a000")};
+            item.SetToLength(10);
+            await sut.SaveSnapshot(item).ConfigureAwait(false);
+            var snapshot = await sut.GetSnapshot(item.AggregateRootId).ConfigureAwait(false);
+            snapshot.ShouldNotBeNull();
+            var cast = snapshot as SnapshotContent;
+            cast.ShouldNotBeNull();
+            cast.Content.Count.ShouldBe(item.Content.Count);
+        }
+
+        [Test]
+        public async Task when_the_snapshot_is_over_table_storage_limits_it_should_save_and_load_to_blob_storage()
+        {
+            var blobStore = new BlobStore(new BlobSettings());
+            var azureBlobStore = new AzureBlobSnapshotStore(blobStore);
+            var azureTableService = new AzureTableService(new AzureTableSettings());
+            var domainEntityTypeBuilder = new DomainEntityTypeBuilder();
+            var sut = new AzureTableSnapshotStore(
+                azureBlobStore,
+                new AzureSnapshotBuilder(azureBlobStore),
+                azureTableService,
+                domainEntityTypeBuilder
+            );
+            var item = new SnapshotContent {AggregateRootId = new Guid("ec3ea6b8-6396-4c95-be2e-4d90f2c23736")};
+            item.SetToLength(99999);
+            await sut.SaveSnapshot(item).ConfigureAwait(false);
+            var snapshot = await sut.GetSnapshot(item.AggregateRootId).ConfigureAwait(false);
+            snapshot.ShouldNotBeNull();
+            var cast = snapshot as SnapshotContent ;
+            cast.ShouldNotBeNull();
+            cast.Content.Count.ShouldBe(item.Content.Count);
+        }
+    }
+}
---
+++ Dauber.Cqrs.EventStore.Azure/AzureBlobSnapshotStore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dauber.Azure.Blob.Contracts;
+using HighIronRanch.Azure.TableStorage;
+using HighIronRanch.Cqrs.EventStore.Azure;
+
+namespace Dauber.Cqrs.EventStore.Azure
+{
+    public class AzureBlobSnapshotStore : IAzureBlobSnapshotStore
+    {
+        private readonly IBlobStore _blobStore;
+
+        public AzureBlobSnapshotStore(IBlobStore blobStore)
+        {
+            _blobStore = blobStore;
+        }
+
+        public async Task Save(string filename, AzureBlobSnapshot.AzureBlobSnapshotPayload snapshot)
+        {
+            await _blobStore.InsertAsync("text/json", filename, snapshot.ToBson()).ConfigureAwait(false);
+        }
+
+        public async Task<AzureBlobSnapshot.AzureBlobSnapshotPayload> Get(string filename)
+        {
+            var blob = await _blobStore.GetAsync(filename, true).ConfigureAwait(false);
+            return blob.Data.FromBson<AzureBlobSnapshot.AzureBlobSnapshotPayload>();
+        }
+    }
+}
