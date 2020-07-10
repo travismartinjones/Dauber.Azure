4c7e88cd73a227adc38713ef6d784807707d5313
kellytrelly <ddrmer@gmail.com>
2020-07-10 10:25:35 -0500

    [Fleet] Add UserId to any fleet event missing this property - https://trello.com/c/XmOhiKlk

---
+++ Dauber.Cqrs.Azure.ServiceBus/TrackedEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Dauber.Cqrs.Contracts;
+using SimpleCqrs.Eventing;
+
+namespace Dauber.Cqrs.Azure.ServiceBus
+{
+    public class TrackedEvent : Event
+    {
+        public Guid UserId { get; set; } 
+        public string UsersName { get; set; }
+    }
+}
