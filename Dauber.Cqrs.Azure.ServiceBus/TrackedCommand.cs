59f18c6448df0a1941b38fafe5704691c261e148
kellytrelly <ddrmer@gmail.com>
2020-07-10 16:17:59 -0500

    Adding TrackedCommand

---
+++ Dauber.Cqrs.Azure.ServiceBus/TrackedCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using HighIronRanch.Azure.ServiceBus.Contracts;
+using SimpleCqrs.Commanding;
+
+namespace Dauber.Cqrs.Azure.ServiceBus
+{
+    public class TrackedCommand : Command
+    {
+        public Guid UserId { get; set; }
+        public string UsersName { get; set; }
+    }
+}
